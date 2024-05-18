826c381dc83ea1a43b9022bb3a68523402a397a8
Muhammad Azan <155957067+Muhammad-Azan-1@users.noreply.github.com>
2024-05-18 22:32:54 +0500

    Basic Product inventory using array

---
+++ main.js
@@ -0,0 +1,88 @@
+let products = [{
+        name: " Circket BAT",
+        price: 2000,
+        inventory: { Stock: 12,
+            colorOptions: ["Brown"]
+        },
+    }, {
+        name: "Foot Balls",
+        price: 1200,
+        inventory: { Stock: 10,
+            colorOptions: ["White"]
+        },
+    }, {
+        name: "Batminton",
+        price: 1000,
+        inventory: { Stock: 8,
+            colorOptions: ["Black"]
+        },
+    },
+];
+/// working if the there is a requirment of a different color then what addition things should we have to add in the object
+for (let i = 0; i < products.length; i++) {
+    function ChangingColor(color, product) {
+        //FOR FIRST OBJECT
+        if (product == products[0]) {
+            if (color === "red") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price + 500;
+                product.inventory.Stock = 5;
+            }
+            else if (color === "blue") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price - 1000;
+                product.inventory.Stock = 25;
+            }
+            else if (color = product.inventory.colorOptions) {
+            }
+            console.log(product);
+            //FOR SECOND OBJECT
+        }
+        else if (product == products[1]) {
+            if (color === "red") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price + 300;
+                product.inventory.Stock = 30;
+            }
+            else if (color === "blue") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price - 300;
+                product.inventory.Stock = 45;
+            }
+            else if (color = product.inventory.colorOptions) {
+            }
+            console.log(product);
+            //FOR THIRD OBJECT
+        }
+        else if (product == products[2]) {
+            if (color === "red") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price + 100;
+                product.inventory.Stock = 8;
+            }
+            else if (color === "blue") {
+                product.inventory.colorOptions.pop();
+                product.inventory.colorOptions.push(color);
+                product.price = product.price - 400;
+                product.inventory.Stock = 10;
+            }
+            else if (color = product.inventory.colorOptions) {
+            }
+            console.log(product);
+        }
+    }
+    ChangingColor("red", products[i]);
+}
+export {};
+// for(let i = 0; i < products.length; i++){
+//     function getProduct(Color:string,){
+//         if(products[i] == products[0]){
+//     }
+//     getProduct("Orange")
+//     }
+//     }
